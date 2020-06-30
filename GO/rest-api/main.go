f0e432a5011b014f2168ee53b419b39776223bdd
Mark Visschers <visschers.m@gmail.com>
2020-06-30 08:58:16 +0200

    basic rest api

--- GO/rest-api/main.go
+++ GO/rest-api/main.go
@@ -1,3 +1,5 @@
+// https://medium.com/the-andela-way/build-a-restful-json-api-with-golang-85a83420c9da
+
 package main
 
 import (
@@ -57,14 +59,64 @@ func getOneEvent(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func getAllEvents(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(events)
+}
+
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
 }
 
+func updateEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(eventID)
+
+	if err != nil {
+		fmt.Fprintf(w, "Error while parsing eventID")
+	}
+
+	var updatedEvent event
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+
+	json.Unmarshal(reqBody, &updatedEvent)
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == id {
+			singleEvent.Title = updatedEvent.Title
+			singleEvent.Description = updatedEvent.Description
+			events = append(events[:i], singleEvent)
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
+}
+
+func deleteEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(eventID)
+
+	if err != nil {
+		fmt.Fprintf(w, "Error while parsing eventID")
+	}
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == id {
+			events = append(events[:i], events[i+1:]...)
+			fmt.Fprintf(w, "The event with ID %v has been deleted successfully", id)
+		}
+	}
+}
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	router.HandleFunc("/event", createEvent).Methods("POST")
+	router.HandleFunc("/events", getAllEvents).Methods("GET")
 	router.HandleFunc("/event/{id:[0-9]+}", getOneEvent).Methods("GET")
+	router.HandleFunc("/event/{id:[0-9]+}", updateEvent).Methods("PATCH")
+	router.HandleFunc("/event/{id:[0-9]+}", deleteEvent).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
